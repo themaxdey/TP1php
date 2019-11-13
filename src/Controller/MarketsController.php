69337c6b5cb162d79835b238a4a10e1504538ca0
themaxdey <maximedery@gmail.com>
2019-11-13 16:08:00 -0500

    Ajout de is auth

--- src/Controller/MarketsController.php
+++ src/Controller/MarketsController.php
@@ -103,4 +103,26 @@ class MarketsController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+        // Les actions 'add' et 'tags' sont toujours autorisés pour les utilisateur
+        // authentifiés sur l'application
+        if (in_array($action, ['add', 'edit', 'delete', 'tags'])) {
+            return true;
+        }
+
+       /* // Toutes les autres actions nécessitent un slug
+        $slug = $this->request->getParam('pass.0');
+        if (!$slug) {
+            return false;
+        }
+
+        // On vérifie que l'asset appartient à l'utilisateur connecté
+        $asset = $this->Assets->findBySlug($slug)->first();
+
+        return $asset->user_id === $user['id'];*/
+    }
+    
 }
